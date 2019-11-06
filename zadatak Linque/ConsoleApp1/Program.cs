5aa345ce707ba5f798d28eb8a3270e1cac64965e
Mihajlo Hornjak <smeceiotpad@gmail.com>
2019-11-06 19:15:44 +0100

    Test komit

---
+++ zadatak Linque/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    public class objekat
+    {
+        public string ime;
+        public string prezime;
+        public string grad;
+        public DateTime date;
+        public objekat() { }
+        public objekat(string ime, string prezime, string grad, DateTime date) 
+        {
+            this.ime = ime;
+            this.prezime = prezime;
+            this.grad = grad;
+            this.date = date;
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Bojana", "Ivana", "Draga", "Milica", "Tina" };
+            List<string> izabrani;
+            
+            izabrani = names.Where(n => n.ToLower().Contains("i")).ToList();
+            var izabraniSaQuery = from ne in names where ne.ToLower().Contains("i") select ne;
+            Console.WriteLine("Ovo je preko QUERY upita");
+            foreach (string n in izabraniSaQuery)
+            {
+                Console.WriteLine(n);
+            }
+            Console.WriteLine("Ovo je preko FLUENT upita");
+            foreach (string n in izabrani)
+            {
+                Console.WriteLine(n);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
